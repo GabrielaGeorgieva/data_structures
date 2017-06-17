968d85e45635f8b838675643161840124931ce74
gabriela1604 <gabriela.georgieva1604@gmail.com>
2017-06-17 20:17:47 +0200

    base functionalities of linked list class

--- python/linked_list.py
+++ python/linked_list.py
@@ -16,15 +16,65 @@ class Node(object):
         if double:
             self.previous = None
 
-    def get_data(self):
-        return self.data
-
-    def get_next(self):
-        return self.next
-
     def set_data(self, new_data):
         self.data = new_data
 
     def set_next(self, new_next):
         self.next = new_next
 
+
+class LinkedList(object):
+    """
+    Linked list implementation
+    """
+    def __init__(self, double=False):
+        self.head = None
+        self.double = double
+
+    def is_double(self):
+        """
+        Is the list single or doubly linked
+        :return: boolean
+        """
+        return self.double
+
+    def is_empty(self):
+        return self.head == None
+
+    def add_item(self, item):
+        new_node = Node(item, double=self.double)
+        new_node.set_next(self.head)
+        self.head = new_node
+
+    def search(self, searched_item):
+        current_node = self.head
+        found = None
+        while current is not None and found is None:
+            if current.data == searched_item:
+                found = current
+            else:
+                current = current.next
+        return found
+
+    def remove(self, remove_item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.data == remove_item:
+                found = True
+            else:
+                previous = current
+                current = current.next
+
+        if previous is None:
+            self.head = current.next
+        else:
+            previous.set_next(current.next)
+
+    def show(self):
+        """
+        to do
+        :return:
+        """
+        pass
